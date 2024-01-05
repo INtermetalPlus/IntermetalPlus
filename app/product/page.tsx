27dcedebbc6eff03d9b04ddff5f7d81419bd3155
Rustam000 <61691677+Rustam000@users.noreply.github.com>
2024-01-06 01:39:36 +0600

    text

---
+++ app/product/page.tsx
@@ -0,0 +1,18 @@
+import styles from './product.scss'
+
+export default function Product{
+    return (
+        <div className='container'>
+            <main className="main">
+                <div className="blog"></div>
+                <div className="main_text">
+                    <p className='text_description'>Скутер Мотоцикл SYM Motors Car Четырехтактный двигатель</p>
+                    <p className='text_model'>Модель: SYM CROX 125 RED</p>
+                    <p className='text_manufacturer'>Производитель: Европа</p>
+                    <p className='text_number'>Серийный номер: 09078568483</p>
+                    <p className='text_products'>Материал изделия: металл, железо, аллюминий</p>
+                </div>
+            </main>
+        </div>
+    )
+}
