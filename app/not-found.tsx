19d05524c3462fe38e5dc0b5242ec43c8d16f760
enoobis <tumin.dan@gmail.com>
2024-01-13 01:34:43 +0600

    Fixed code in error.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,9 +8,9 @@ import Link from 'next/link';
 const Custom404: React.FC = () => {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <img src={"/robot.png"} alt="404 Error: Page Not Found" style={{ width: '300px', margin: '0 auto' }} />
-      <img src={"/404.png"} alt="404 Error: Page Not Found" style={{ width: '250px', margin: '0 auto', paddingBottom: '40px' }} />
-      <p style={{ fontSize: '40px' , paddingTop: '85px', fontWeight:400, lineHeight:'46.88px' , color:'#525252'}}>Страница не найдена</p>
+      <Image src={"/robot.png"} alt="404 Error: Page Not Found" width={513} height={326} style={{margin: '0 auto'}} />
+      <Image src={"/404.png"} alt="404 Error: Page Not Found" width={380} height={200} style={{margin: '0 auto', marginBottom: 60 }} />
+      <p style={{ fontSize: '40px' , paddingTop: '65px', fontWeight:400, lineHeight:'46.88px' , color:'#525252'}}>Страница не найдена</p>
       <p style={{ fontSize: '40px' , paddingBottom: '85px',fontWeight:400, lineHeight:'46.88px' , color:'#525252'}}>
       <Link href="/" style={{textDecoration: 'none', color: '#000' }}>
       Вернуться на главную страницу</Link></p>
