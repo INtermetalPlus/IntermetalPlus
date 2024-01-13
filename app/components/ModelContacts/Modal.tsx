93cf889e7196853d6a0496020326ab8a7bcad176
Nazzemax <hvan1998@inbox.ru>
2024-01-14 04:30:56 +0600

    feat:implemented contact form and connected it to backend

--- app/components/ModelContacts/Modal.tsx
+++ app/components/ModelContacts/Modal.tsx
@@ -1,20 +1,32 @@
+// Import necessary modules and types
 import React, { useState, useEffect } from 'react';
 import styles from './contacts.module.scss';
 import Image from 'next/image';
-import { getContacts } from './getContacts';
+import { getContacts } from '@/app/lib/data';
 
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
+// Define a type for the contact information
+interface Contact {
+  Phone_number: string;
+  number_WhatsApp: string;
+  Email: string;
+}
+
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
-  const [contacts, setContacts] = useState(null); // or set the initial state appropriately
+  const [contacts, setContacts] = useState<Contact | null>(null);
 
   useEffect(() => {
     const fetchContacts = async () => {
-      const data = await getContacts();
-      setContacts(data);
+      try {
+        const data: Contact = await getContacts();
+        setContacts(data);
+      } catch (error) {
+        console.error('Error fetching contacts:', error);
+      }
     };
 
     if (isOpen) {
@@ -26,28 +38,28 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
 
   return (
     <div className={styles.modalOverlay}>
-    <div className={styles.modal}>
-      <div className={styles.modalHeader}>
-        <button className={styles.closeButton} onClick={onClose}>
-          <img src="/close.png" alt="Close" />
-        </button>
-      </div>
-      <div className={styles.modalContent}>
-        <div className={styles.contactItem}>
-          <img src="/bphone.png" alt="Phone" />
-          <span>+996 773 505 000</span>
+      <div className={styles.modal}>
+        <div className={styles.modalHeader}>
+          <button className={styles.closeButton} onClick={onClose}>
+            <Image width={25} height={25} src="/close.png" alt="Close" />
+          </button>
         </div>
-        <div className={styles.contactItem}>
-          <img src="/whatsapp.png" alt="WhatsApp" />
-          <span>+996 773 505 000</span>
-        </div>
-        <div className={styles.contactItem}>
-          <img src="/mail.png" alt="Email" />
-          <span>intermetalplus33@gmail.com</span>
+        <div className={styles.modalContent}>
+          <div className={styles.contactItem}>
+            <Image width={25} height={25} src="/bphone.png" alt="Phone" />
+            <span>{contacts?.Phone_number}</span>
+          </div>
+          <div className={styles.contactItem}>
+            <Image width={25} height={25} src="/whatsapp.png" alt="WhatsApp" />
+            <span>{contacts?.number_WhatsApp}</span>
+          </div>
+          <div className={styles.contactItem}>
+            <Image width={25} height={25} src="/mail.png" alt="Email" />
+            <span>{contacts?.Email}</span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,6 +10,17 @@ export async function getProducts() {
     }
 }
 
+export async function getContacts() {
+    try{
+        const res = await fetch('http://146.190.140.32/api/v1/contact/')
+        const data = await res.json()
+        return data
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+
 export async function getProductById(id:number | string) {
     try {
         const res = await fetch(`http://146.190.140.32/api/v1/products/${id}/`)
