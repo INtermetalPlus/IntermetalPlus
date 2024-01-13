709a1fbf6d500dd57728ba087e872089e2ac0a99
Эмир Ынтымак уулу <sheraliev270704@gmail.com>
2024-01-13 23:47:55 +0600

    add react-hook-form

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -41,8 +41,6 @@ export default function Product() {
             </li>
           </ol>
         </section>
-        <button className={styles.callUs} onClick={handleOpenModal}> Связаться с нами </button>
-        <Modal isOpen={isModalOpen} onClose={handleCloseModal} />
         <section className={styles.review}>
           <h3 className={styles.review_title}>Видео обзор</h3>
           <iframe width="1040" height="711" src="https://www.youtube.com/embed/1aQ5XpHGMu0?si=RMoMFp3UZSHGmy0x" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
